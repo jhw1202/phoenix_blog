5aea05d2d6e65ea9de5daa6626fa9539a08fb81d
Philip Woo <jhw1202@gmail.com>
2016-07-03 18:01:24 -0700

    remove pry

--- web/controllers/user_controller.ex
+++ web/controllers/user_controller.ex
@@ -43,7 +43,7 @@ defmodule Pxblog.UserController do
   def update(conn, %{"id" => id, "user" => user_params}) do
     user = Repo.get!(User, id)
     changeset = User.changeset(user, user_params)
-    IEx.pry
+
     case Repo.update(changeset) do
       {:ok, user} ->
         conn
