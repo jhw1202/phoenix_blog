e9c09404a496d1f1c54e5d28ddcdd98d5b2a6290
Philip Woo <jhw1202@gmail.com>
2016-07-13 19:32:19 -0700

    compiler warning unused arg

--- web/controllers/session_controller.ex
+++ web/controllers/session_controller.ex
@@ -26,7 +26,7 @@ defmodule Pxblog.SessionController do
     |> redirect(to: page_path(conn, :index))
   end
 
-  defp sign_in(user, password, conn) when is_nil(user) do
+  defp sign_in(user, _password, conn) when is_nil(user) do
     conn
     |> put_flash(:error, "Invalid username/password combination")
     |> redirect(to: page_path(conn, :index))
